54aceb392c2f4cfc5bec7c3e3efda15d1d824be2
Yaroslav Sedyshev <jareksedy@icloud.com>
2024-01-28 18:58:52 +0600

    Initial Commit

---
+++ Mote Watch App/MoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoteApp.swift
+//  Mote Watch App
+//
+//  Created by Ярослав on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Mote_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mote/MoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoteApp.swift
+//  Mote
+//
+//  Created by Ярослав on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
