7bd3f8f4d4c5684b1448552efb89f78d00d57ac2
Yaroslav Sedyshev <yaroslavse@halykbank.kz>
2024-01-29 12:49:43 +0600

    Minor improvements.

--- Mote/ContentView.swift
+++ Mote/ContentView.swift
@@ -12,15 +12,17 @@ struct ContentView: View {
     @State private var message: String = ""
     var body: some View {
         VStack {
-            Text("Received message: \(viewModel.message)")
-                .fixedSize(horizontal: false, vertical: true)
-            Divider()
-            TextField("Message to paired Apple Watch", text: $message)
-            Button("Send", action: {
-                let data = ["message": message]
-                message = ""
-                viewModel.session.sendMessage(data, replyHandler: nil)
-            })
+            ScrollView {
+                Text("Received message: \(viewModel.message)")
+                    .fixedSize(horizontal: false, vertical: true)
+                Divider()
+                TextField("Message to paired Apple Watch", text: $message)
+                Button("Send", action: {
+                    let data = ["message": message]
+                    message = ""
+                    viewModel.session.sendMessage(data, replyHandler: nil)
+                })
+            }
         }
         .padding()
     }
